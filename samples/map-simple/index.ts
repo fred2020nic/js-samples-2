954cc332aa5b15bcea9a1aa189d17552e380c39d
fred2020nic <60634761+fred2020nic@users.noreply.github.com>
2023-04-13 23:28:36 -0600

    first commit

---
+++ samples/map-simple/index.ts
@@ -0,0 +1,20 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// [START maps_map_simple]
+let map: google.maps.Map;
+async function initMap(): Promise<void> {
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  map = new Map(document.getElementById("map") as HTMLElement, {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+initMap();
+// [END maps_map_simple]
+export {};
